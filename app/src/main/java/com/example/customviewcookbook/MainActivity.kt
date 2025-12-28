9c70116ad959d1258c7453b891aa7a1fc55babe5
GuerraAna <ana.guerrads@hotmail.com>
2025-12-27 22:09:58 -0300

    init banner view

---
+++ app/src/main/java/com/example/customviewcookbook/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.customviewcookbook
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.customviewcookbook.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        // Infla o layout usando ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Acessa a view através do binding
+        val banner = binding.bannerHighlight
+
+        // Configura os textos
+        banner.setTitle("Novo Título")
+        banner.setDescription("Nova descrição")
+        banner.setStrokeColor(ContextCompat.getColor(this, R.color.red))
+
+        // Define listener para o botão de fechar
+        banner.setCloseButtonVisibility(true)
+        banner.onCloseClickListener = {
+            banner.visibility = View.GONE
+            // Ou remova a view: (banner.parent as? ViewGroup)?.removeView(banner)
+        }
+    }
+}
